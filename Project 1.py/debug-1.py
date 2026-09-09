e08e062c1319d0252df52ad6e72f1d5c4ff1173d
GuadalupeMaria1 <Guadalupe.maria1935@gmail.com>
2026-09-09 14:52:10 -0600

    adding debug

--- Project 1.py/debug-1.py
+++ Project 1.py/debug-1.py
@@ -4,10 +4,12 @@
 # This program should add three numbers together.
 
 total = 0
-num1 = input("What's the first number? >")
+num1 = int(input("What's the first number? >"))
 total = num1
-num2 = input("What's the second number? >")
+num2 = float(input("What's the second number? >"))
 total = num2
-num3 = input("What's the third number? >")
-total = num3
+num3 = int(input("What's the third number? >"))
+total = num3+num2+num1
 print(f"Total is: {total}")
+
+#In the program I had to change the f-string to an interger or also float and add input with an extra parenthisis to help me add the total instead of just giving me a string of numbers it will give me the equal number when added together. Another thing is adding all numbers as well as the addition sign to make sure that it adds the three numbers together. 
