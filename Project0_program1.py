836d9957171bcd59d550f0b3ac1f792f1b9ea3fa
GuadalupeMaria1 <guadalupe.maria1935@gmail.com>
2026-09-24 10:35:54 -0600

    Project 1 Thomas P

--- Project0_program1.py
+++ Project0_program1.py
@@ -1,1 +1,8 @@
-#maria
+#Maria Gomez-Arroyo
+
+age_text=input("How old are you? ")
+age_number=int(age_text)
+# Using the int it will make them intergers and help formulate the math instead of a string.
+print(age_number,type(age_number))
+# Using type will help the system know which variable we are going to be using
+print(f"You will be {age_number+10}")
--- Project0_program2.py
+++ Project0_program2.py
@@ -1,10 +1,10 @@
-fav_bball='Warriors'
+fav_fruit='Watermelon'
 leastfav_bball='Wizards'
 #give variable value
 team_text=input('What is your favorite team? ')
 #Prints out next line after inputting fav team
-print(f'Mine too! I love the {fav_bball}')
+print(f'Mine too! I love the {fav_fruit}')
 #use f string to print something after inputting if you want it to give a sentence back
 team_text=input('Whats your least favorite team? ')
 print(f'I dont like the {leastfav_bball}, but I do like their new player AJ')
-= sign means trying to give it a value, dont use with print 
+# sign means trying to give it a value, dont use with print 
